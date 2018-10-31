96a4ea8956a657f3125839b7c661ee490f97c887
Farsheed Ibram <farsheed_ibram@hotmail.co.uk>
2018-10-31 21:05:09 +1300

    Initial commit

---
+++ script.js
@@ -0,0 +1,146 @@
+var openFile = function(event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function(){
+      var text = reader.result;
+      console.log(reader.result.substring(0, 200));
+      var data = text;
+    var xml = "";
+
+    if (data !== null && data.trim().length !== 0) {
+
+        try {
+            xml = $.parseXML(data);
+        } catch (e) {
+            throw e;
+        }
+
+        var x2js = new X2JS();
+
+        data = x2js.xml2json(xml);
+        jsonToCsvByJson(data);
+        
+    }
+
+    };
+    reader.readAsText(input.files[0]);
+
+    
+  };
+  
+
+
+function loadFile(){
+    var xml = document.getElementById("myFile").files[0];
+    console.log(xml);
+    var fr = new FileReader();
+    fr.onload = function(e) {
+    // e.target.result should contain the text
+    };
+    fr.readAsText(file);
+}
+
+function xmlTocsv() {
+
+    var data = $("#xmlArea").val();
+
+    var xml = "";
+
+    if (data !== null && data.trim().length !== 0) {
+
+        try {
+            xml = $.parseXML(data);
+        } catch (e) {
+            throw e;
+        }
+
+        var x2js = new X2JS();
+
+        data = x2js.xml2json(xml);
+        jsonToCsvByJson(data);
+        
+    }
+}
+
+function jsonToCsvByJson(data, returnFlag) {
+
+    arr = [];
+    flag = true;
+
+    var header = "";
+    var content = "";
+    var headFlag = true;
+
+    try {
+
+        var type1 = typeof data;
+
+        if (type1 != "object") {
+            data = processJSON($.parseJSON(data));
+        } else {
+            data = processJSON(data);
+        }
+
+    } catch (e) {
+        if (returnFlag === undefined || !returnFlag) {
+            console.error("Error in Convert to CSV");
+        } else {
+            console.error("Error in Convert :" + e);
+        }
+        return false;
+    }
+
+    $.each(data, function(k, value) {
+        if (k % 2 === 0) {
+            if (headFlag) {
+                if (value != "end") {
+                    header += value + ",";
+                } else {
+                    // remove last colon from string
+                    header = header.substring(0, header.length - 1);
+                    headFlag = false;
+                }
+            }
+        } else {
+            if (value != "end") {
+                var temp = data[k - 1];
+                if (header.search(temp) != -1) {
+                    content += value + ",";
+                }
+            } else {
+                // remove last colon from string
+                content = content.substring(0, content.length - 1);
+                content += "\n";
+            }
+        }
+
+    });
+
+    if (returnFlag === undefined || !returnFlag) {
+        $("#csvArea").val(header + "\n" + content);
+    } else {
+        return (header + "\n" + content);
+    }
+}
+
+function processJSON(data) {
+
+    $.each(data, function(k, data1) {
+
+        var type1 = typeof data1;
+
+        if (type1 == "object") {
+
+            flag = false;
+            processJSON(data1);
+            arr.push("end");
+            arr.push("end");
+
+        } else {
+            arr.push(k, data1);
+        }
+
+    });
+    return arr;
+}
